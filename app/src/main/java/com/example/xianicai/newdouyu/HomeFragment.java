68ed715b511ba6babab767dc15888c32aca7141b
XiaNiCai <zhanglibinit@163.com>
2017-11-20 18:04:32 +0800

    首页布局的添加

--- app/src/main/java/com/example/xianicai/newdouyu/HomeFragment.java
+++ app/src/main/java/com/example/xianicai/newdouyu/HomeFragment.java
@@ -1,14 +1,26 @@
 package com.example.xianicai.newdouyu;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.xianicai.newdouyu.base.view.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * Created by Zhanglibin on 2017/11/17.
  */
 
 public class HomeFragment extends BaseFragment {
+    @BindView(R.id.tv_search)
+    TextView mTvSearch;
+    @BindView(R.id.tabLayout)
+    TabLayout mTabLayout;
+    @BindView(R.id.view_pager)
+    ViewPager mViewPager;
+
     public static HomeFragment newInstance() {
         return new HomeFragment();
     }
